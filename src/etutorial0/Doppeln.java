4125fdaeaabff65d69972c347e3ae5f7e35d7889
alex wüst <alex@4row.com>
2023-08-22 20:38:14 +0200

    comment added ZHAW CAS OOP

---
+++ src/etutorial0/Doppeln.java
@@ -0,0 +1,11 @@
+package etutorial0;
+
+public class Doppeln {
+    public static void main(String[] args) {
+        //
+        int zahl;
+        zahl = 5;
+        zahl = zahl * 2;
+        System.out.println(zahl);
+    }
+}
