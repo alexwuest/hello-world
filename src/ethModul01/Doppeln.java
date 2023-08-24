464f7907be6dac78b60b908171b26bcd0809b856
alex wüst <alex@4row.com>
2023-08-24 09:48:46 +0200

    init

---
+++ src/ethModul01/Doppeln.java
@@ -0,0 +1,20 @@
+package ethModul01;
+
+public class Doppeln {
+    //A fixed number in the variable zahl will be doubled
+    public static void main(String[] args) {
+        int zahl;
+        int doppelte_zahl;
+        zahl = 5;
+        doppelte_zahl = zahl * 2;
+
+        //Printing out the original number and the new doubled number
+        System.out.println("Ursprüngliche Zahl: "+ zahl);
+        System.out.println("Verdoppelte Zahl: "+ doppelte_zahl);
+    }
+}
+
+
+
+
+
