e68a23c48de55e223e8ec6c203068a4d4356cce0
alex wüst <alex@4row.com>
2023-08-24 16:45:43 +0200

    comit

---
+++ src/ethModul01/Chiffre.java
@@ -0,0 +1,41 @@
+package ethModul01;
+
+import java.util.Scanner;
+
+public class Chiffre {
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        // Ceasar-Verschlüsselung für ein beliegiges Zeichen
+        // Deklarieren Variablen
+        String zeichen;
+        int chiff_schluessel;
+        char buchstabe;
+        int buchstabe_ascii;
+        int buchstabe_ascii_chiff;
+        char buchstabe_chiff;
+
+        // Die beiden Eingaben durch den Benutzer
+        System.out.println("Geben Sie ein Zeichen ein!");
+        zeichen = eingabe.next();
+
+        System.out.println("Geben Sie einen Schlüssel ein!");
+        chiff_schluessel = eingabe.nextInt();
+
+        // Ausgabe der Eingabe
+        System.out.println("Sie haben " + zeichen + " und " + chiff_schluessel + " eingegeben");
+
+        // String in char umwandeln und nur 1 Zeichen verwenden.
+        buchstabe = zeichen.charAt(0);
+
+        // Buchstabe in ASCII Ordnungszahl umwandeln
+        buchstabe_ascii = (int) buchstabe;
+
+        // Buchstabe um Chiffrierschlüssel verschieben durch Subtraktion
+        buchstabe_ascii_chiff = buchstabe_ascii - chiff_schluessel;
+
+        // Buchstabe_ascii_chiffriert umwandeln in ASCII Zeichen und Ausgabe
+        buchstabe_chiff = (char) buchstabe_ascii_chiff;
+        System.out.println("Das verschlüsselte Zeichen ist: " + buchstabe_chiff);
+
+    }
+}
