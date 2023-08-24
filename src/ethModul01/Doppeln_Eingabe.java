dbc96963d4f37e8f8cad2979e1cf0c9e4e76860f
alex wüst <alex@4row.com>
2023-08-24 11:19:51 +0200

    intial

---
+++ src/ethModul01/Doppeln_Eingabe.java
@@ -0,0 +1,19 @@
+package ethModul01;
+
+//Import Scanner für Benutzereingabe
+import java.util.Scanner;
+public class Doppeln_Eingabe {
+
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+
+        //Deklaration der Variablen als int
+        int zahl;
+        int doppelte_zahl;
+        System.out.println("Bitte geben Sie den Wert ein, welcher verdoppelt werden soll:");
+        zahl = eingabe.nextInt();
+        doppelte_zahl = zahl * 2;
+        System.out.println("Die Eingabe lautete: "+zahl);
+        System.out.println("Der errechnete Wert lautet: "+doppelte_zahl);
+    }
+}
