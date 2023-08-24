ce91ed7588580269099f492b9fb64e33b8d474ea
alex wüst <alex@4row.com>
2023-08-24 13:35:53 +0200

    comit

--- src/Java/Notes.java
+++
@@ -1,6 +0,0 @@
-/* Dies sind meine persönlichen Notizen mit wichtigen Codefragmenten die mir während meiner Ausbildung an der ZHAW begegnet sind. */
-// Nur eine Zeile kommentiert ;-)
-
-// Standardroutine die jede Klasse benötigt um von extern ausgeführt werden zu können.
-public static void main(String[]args){
-        }
