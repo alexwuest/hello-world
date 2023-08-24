79ac1e4f1d636f5a9374e81d7e7e411cb7c2973a
alex wüst <alex@4row.com>
2023-08-24 13:11:37 +0200

    Rundung der Fläche und Umfang eingefügt

--- src/ethModul01/Kreisberechnung.java
+++ src/ethModul01/Kreisberechnung.java
@@ -7,27 +7,27 @@ public class Kreisberechnung {
     public static void main(String[] args) {
         Scanner eingabe = new Scanner(System.in);
 
-        //Deklarierung der Variablen
+        // Deklarierung der Variablen
         double radius;
         double umfang;
         double flaeche;
         final double pi;
         pi = 3.14159265359;
 
-        //Radius Eingabe durch Benutzer
+        // Radius Eingabe durch Benutzer
         System.out.println("Bitte geben Sie den Radius des Kreises ein:");
         radius = eingabe.nextDouble();
 
-        //Berechnung der Kreisfläche A = radius * radius * pi
-        //Berechnung des Kreisumfangs U = 2 * r * pi
+        // Berechnung der Kreisfläche A = radius * radius * pi
+        // Berechnung des Kreisumfangs U = 2 * r * pi
         flaeche = radius * radius * pi;
         umfang = 2 * radius * pi;
 
-        //Werte auf 2 Kommastellen runden
+        // Werte auf 2 Kommastellen runden
         flaeche = Math.round(flaeche * 100) / 100.0;
         umfang = Math.round(umfang * 100) / 100.0;
 
-        //Ausgabe des Werts
+        // Ausgabe des Werts
         System.out.println("Die Fläche des Kreises ist " + flaeche + " der Umfang des Kreises ist " + umfang);
     }
 }
