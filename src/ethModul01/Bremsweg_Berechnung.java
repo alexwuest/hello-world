7c1390f8ef7b3d78e9de8daf10a3701af249979c
alex wüst <alex@4row.com>
2023-08-24 17:02:11 +0200

    comit

---
+++ src/ethModul01/Bremsweg_Berechnung.java
@@ -0,0 +1,29 @@
+package ethModul01;
+import java.util.Scanner;
+public class Bremsweg_Berechnung {
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+
+        // Deklarieren der Variablen
+        float geschwindigkeit;
+        float reaktionsweg;
+        float bremsweg;
+        float anhalteweg;
+
+        // Eingabe der Geschwindigkeit durch Benutzer
+        geschwindigkeit = eingabe.nextInt();
+
+        // Berechnung von Reaktionsweg
+        reaktionsweg = geschwindigkeit / 10 * 3;
+
+        // Berechnung von Bremsweg
+        bremsweg = (geschwindigkeit/10)*(geschwindigkeit/10);
+
+        // Brechnung von Anhalteweg
+        anhalteweg = reaktionsweg + bremsweg;
+
+        // Ausgabe der Resultate
+        System.out.println("Der Anhalteweg beträgt " + anhalteweg + "m");
+        System.out.println("Dieser setzt sich zusammen aus " + reaktionsweg + "m Reaktionsweg und " + bremsweg + "m Anhalteweg");
+    }
+}
